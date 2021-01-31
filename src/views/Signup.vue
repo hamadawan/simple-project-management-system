676869450615cd0e6b8c422174d9ae530a15182d
HamadAwan <=>
2021-01-31 23:24:37 +0500

    firebase login with email

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,34 +1,59 @@
 <template>
     <div class="vue-signup">
-        <form>
+        <div>
             <h3>Sign Up</h3>
             <div class="form-group">
                 <label>Full Name</label>
-                <input type="text" class="form-control form-control-lg"/>
+                <input type="text"  class="form-control form-control-lg"/>
             </div>
             <div class="form-group">
                 <label>Email address</label>
-                <input type="email" class="form-control form-control-lg" />
+                <input type="email" v-model="email" class="form-control form-control-lg" />
             </div>
             <div class="form-group">
                 <label>Password</label>
-                <input type="password" class="form-control form-control-lg" />
+                <input type="password" v-model="password" class="form-control form-control-lg" />
             </div>
-            <button type="submit" class="btn btn-dark btn-lg btn-block">Sign Up</button>
+            <button v-on:click="submit()" class="btn btn-dark btn-lg btn-block">Sign Up</button>
             <p class="forgot-password text-right">
                 Already registered 
                 <router-link to="/login">sign in?</router-link>
             </p>
-        </form>
+        </div>
     </div>
 </template>
 
 <script>
+import firebase from 'firebase'
 
 export default {
+    data()
+    {
+        return {
+            email:'',
+            password:''
+        }
+    },
+
     components:{
     },
+
     computed:{
+    },
+
+    methods:{
+        submit(){
+            console.log("working")
+            firebase
+            .auth()
+            .createUserWithEmailAndPassword(this.email, this.password)
+            .then(data => {
+                console.log(data.user)
+            })
+            .catch(err => {
+                console.log(err)
+            });
+        }
     }
 }
 </script>
